5931af40b41a6b5e705b9cd55a2c06a4c319bb5f
KaushalSahu <kaushal.raj.sahu@gmail.com>
2019-02-16 01:14:16 +0530

    Prime1

---
+++ Python/q3.py
@@ -0,0 +1,15 @@
+import math
+
+t = int(input())
+for i in range(t):
+    n1, n2 = input().split(" ")
+    n1 = int(n1)
+    n2 = int(n2)
+    for i in range(n1, n2+1):
+        c = 0
+        for j in range(2, (int)(math.sqrt(i))+1):
+            if (i % 2 ==0 or i % j == 0):
+                c = c + 1
+                break
+        if (c == 0 and i != 1):
+            print (i)
