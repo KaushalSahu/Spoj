21109dad258161e8831c8fb2aa8a88cb73b2dd65
KaushalSahu <kaushal.raj.sahu@gmail.com>
2019-02-15 22:55:53 +0530

    prime1

--- q3.cpp
+++ q3.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
-#include<math.h>
 using namespace std;
 
 int main(){
     int t, n1, n2, i, j, c;
     cin >> t;
+
     while(t--){
         cin >> n1;
         cin >> n2;
         for(i = n1; i <= n2; i++){
             c = 0;
-            for(j = 2; j <= sqrt(i); j++){
-                 if(i % j == 0)
+            for(j = 2; j * j <= i; j++){
+                 if(i % j == 0){
                     c++;
+                    break;
+                 }
             }
-            if(c == 0)
+            if(c == 0 && i !=1)
             cout << i << "\n";
         }
     }
