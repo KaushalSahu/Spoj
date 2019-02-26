d40ce86687325afcbb4e2c9f778f5aa99e400a03
KaushalSahu <kaushal.raj.sahu@gmail.com>
2019-02-26 10:53:44 +0530

    PRIME1

--- Python/q3.py
+++ Python/q3.py
@@ -7,7 +7,8 @@ for i in range(t):
     n2 = int(n2)
     for i in range(n1, n2+1):
         c = 0
-        if(i%n1==0)continue;
+        if(i%n1==0):
+            continue
         for j in range(2, (int)(math.sqrt(i))+1):
             if (i % 2 ==0 or i % j == 0):
                 c = c + 1
